e55d5633e387f52c39595a3a1d3c523c5734c496
Rahul Kalra <rkalra247@gmail.com>
2017-06-09 06:18:52 -0700

    another solution for problem #16

--- solutions/16.js
+++ solutions/16.js
@@ -1,4 +1,28 @@
-//Manik Sachdeva: msach22
+// Rahul Kalra
+// should retirn true if the input has duplicates values and false if it doesn't
+
+const solution = (array) => {
+  let data = {};
+  for(let i = 0; i < array.length; i++){
+    if(!(data[array[i]])){
+      data[array[i]] = 1;
+    }
+    else{
+      data[array[i]]++;
+    }
+  }
+  for(let key in data){
+    if((data[key]) > 1){
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = {solution};
+
+/*
+ //Manik Sachdeva: msach22
 // should return true if the input array has duplicate values and false if it doesn't
 
 const solution = (array) => {
@@ -15,3 +39,4 @@ const solution = (array) => {
 module.exports = {
   solution
 };
+*/
--- test/16.js
+++ test/16.js
@@ -3,15 +3,15 @@ let solution = require('../solutions/16').solution;
 // solution = require('./yourSolution').solution;
 
 describe('check duplicates', () => {
-  it('should return true since array has duplicates', () => {
+  it.only('should return true since array has duplicates', () => {
     const array = [1,2,3,3,4];
     expect(solution(array)).to.be.true;
   });
-  it('should return false because array does not have duplicates', () => {
+  it.only('should return false because array does not have duplicates', () => {
     const array = [1,2,3,4];
     expect(solution(array)).to.be.false;
   });
-  it('should return false because array is empty', () => {
+  it.only('should return false because array is empty', () => {
     const array = [];
     expect(solution(array)).to.be.false;
   });
