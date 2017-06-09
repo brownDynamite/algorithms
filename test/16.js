6f9e052c9020260e68bd263b6cd21b90fdb2b58f
Rahul Kalra <rkalra247@gmail.com>
2017-06-09 07:08:46 -0700

    removed .only for the test

--- test/16.js
+++ test/16.js
@@ -3,15 +3,15 @@ let solution = require('../solutions/16').solution;
 // solution = require('./yourSolution').solution;
 
 describe('check duplicates', () => {
-  it.only('should return true since array has duplicates', () => {
+  it('should return true since array has duplicates', () => {
     const array = [1,2,3,3,4];
     expect(solution(array)).to.be.true;
   });
-  it.only('should return false because array does not have duplicates', () => {
+  it('should return false because array does not have duplicates', () => {
     const array = [1,2,3,4];
     expect(solution(array)).to.be.false;
   });
-  it.only('should return false because array is empty', () => {
+  it('should return false because array is empty', () => {
     const array = [];
     expect(solution(array)).to.be.false;
   });
