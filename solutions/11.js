05748d136e63e4ca6ca394b310cfb7aac67dac5c
Rahul Kalra <rkalra247@gmail.com>
2017-06-20 16:47:33 -0700

    new solution and test for #problem13

--- solutions/11.js
+++ solutions/11.js
@@ -1,27 +1,24 @@
 //Rahul Kalra
 //Return repeating elements of an array
 
-const solution = (array) => {
+const solution2 = (inputArr) => {
   let newArray = [];
-  let data = [];
-  for(let i = 0; i < array.length; i++){
-    if(!(data[array[i]])){
-      data[array[i]] = 1;
+  let counter = [];
+  for(let i = 0; i < inputArr.length; i++){
+    if(!(counter[inputArr[i]])){
+      counter[inputArr[i]] = 1;
     }else{
-      data[array[i]]++; 
+      counter[inputArr[i]]++; 
     }
   }
-  for(let key in data){
-    if(data[key] > 1){
+  for(let key in counter){
+    if(counter[key] > 1){
       newArray.push(Number(key));
     }
   }
   return newArray;
 };
-
-module.exports = {solution};
-
-/*// vdewinter
+// vdewinter
 // Create a function that returns only the values of an array that are repeated
 // Input: [1,8,8,8,7,7,1,3]
 // Output: [8,7,1]
@@ -42,7 +39,8 @@ const solution = (input) => {
 };
 
 module.exports = {
-  solution
+  solution,
+  solution2 
 };
 
-*/
+
--- test/11.js
+++ test/11.js
@@ -1,21 +1,16 @@
 const expect = require('chai').expect;
 let solution = require('../solutions/11').solution;
+    solution2 = require('../solutions/11').solution2;
 
 describe('repeating elements', () => {
-  it.only('should return repeating elements of an array', () => {
-    expect(solution([1,8,8,8,7,7,1,3])).to.deep.equal([1,7,8]);
+  it('should return repeating elements of an array', () => {
+    expect(solution2([1,8,8,8,7,7,1,3])).to.deep.equal([1,7,8]);
   });
 });
-
-/*
-const expect = require('chai').expect;
-let solution = require('../solutions/11').solution;
-// solution = require('./yourSolution').solution;
-
 describe('repeating elements', () => {
   it('should return repeating elements of an array', () => {
     const result = solution([1,8,8,8,7,7,1,3]);
     expect(result).to.deep.equal([8,7,1]);
   });
 });
-*/
+
