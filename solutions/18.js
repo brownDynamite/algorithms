e45fffa2d25474bc091da84c43b8e86153856be3
Rahul Kalra <rkalra247@gmail.com>
2017-06-19 17:56:35 -0700

    fixed all the merge conflicts

--- solutions/18.js
+++ solutions/18.js
@@ -1,39 +1,27 @@
 // Rahul Kalra
-// Return the missing number in an input array of consecutively increasing number
+// Return the  missing number in an input array of consecutively increasing number
 
-const solution = (array) =>{
-  let newArray = [];
-  for(let i = 0; i < array.length; i++){
-    if(!((array[i + 1]) - (array[i])) === 1){
-      newArray.push((array[i]) + 1);
+const solution3 = (array) =>{
+  let missNum = 0;
+  for( let i = 0; i < array.length; i++){
+    if( array[i+1]){
+      if( (array[i+1] - array[i]) > 1){
+        missNum = array[i] + 1;
+        return missNum;
+      } 
     }
   }
-  return newArray;
+  return null;
 };
 
-module.exports = {solution};
-
-
-// Find missing number in array
-// Return the missing number in an input array of consecutively
-// increasing numbers.
-// input: [5,6,8,9]
-// output: 7
-
 // Zach Nagatani
+// * Finds and returns the missing number from an array of consecutively increasing numbers
+// * @param {Number[]} arr - An array of consecutively increasing numbers
+// * @returns {Number|null} missingNum - the number missing from the input array or null if none is found
 
-/*
- * Finds and returns the missing number from an array of consecutively
- * increasing numbers
- * @param {Number[]} arr - An array of consecutively increasing numbers
- * @returns {Number|null} missingNum - the number missing from the
- * input array or
- * null if none is found
- */
 
-/*const solution1 = arr => {
+const solution1 = arr => {
     let missingNum = null,
->>>>>>> another solution for problem #18
         i = 0;
 
     while (!missingNum && i < arr.length - 1) {
@@ -46,7 +34,7 @@ module.exports = {solution};
 
     return missingNum;
 };
-
+// Zach Nagatani
 /*
  * Finds and returns the missing number from an array of consecutively
  * increasing numbers
@@ -56,20 +44,20 @@ module.exports = {solution};
  * null if none is found
  */
 
-// const solution2 = arr => {
-//  let i = 1;
-//
-//  while (i < arr.length && arr[i - 1] + 1 === arr[i]) {
-//      i++;
-//  }
-//
-//  return i === arr.length ? null : arr[i - 1] + 1;
-//}
+
+ const solution2 = arr => {
+  let i = 1;
+
+  while (i < arr.length && arr[i - 1] + 1 === arr[i]) {
+      i++;
+  }
+  return i === arr.length ? null : arr[i - 1] + 1;
+}
 
 
-
-//module.exports = {
-//  solution1,
-//  solution2
-//};
+module.exports = {
+  solution1,
+  solution2,
+  solution3
+};
 
--- test/18.js
+++ test/18.js
@@ -1,35 +1,43 @@
 const expect = require('chai').expect;
 let solution1 = require('../solutions/18').solution1;
-    solution2 = require('../solutions/18').solution2;
+solution2 = require('../solutions/18').solution2;
+solution3 = require('../solutions/18').solution3;
 
 describe('Find missing number in array', () => {
-  it('should return 7 for [5,6,8,9]', () => {
-    const result1 = solution1([5, 6, 8, 9]);
-          result2 = solution2([5, 6, 8, 9]);
-    expect(result1).to.equal(7);
-    expect(result2).to.equal(7);
-
+it.only('should return 7 for [5,6,8,9]', () => {
+  const result1 = solution1([5,6,8,9]),
+        result2 = solution2([5,6,8,9]);
+        result3 = solution3([5,6,8,9]);
+  expect(result1).to.equal(7);
+  expect(result2).to.equal(7);
+  expect(result3).to.equal(7);
 });
 
-  it('should return 94 for [89,90,91,92,93,95]', () => {
-    const result1 = solution1([89, 90, 91, 92, 93, 95]);
-          result2 = solution2([89, 90, 91, 92, 93, 95]);
-    expect(result1).to.equal(94);
-    expect(result2).to.equal(94);
-  });
+it.only('should return 94 for [89,90,91,92,93,95]', () => {
+const result1 = solution1([89,90,91,92,93,95]),
+      result2 = solution2([89,90,91,92,93,95]);
+      result3 = solution3([89,90,91,92,93,95]);
+expect(result1).to.equal(94);
+expect(result2).to.equal(94);
+expect(result3).to.equal(94);
+});
 
-  it('should return null if there is no missing number', () => {
-    const result1 = solution1([1, 2, 3, 4, 5]);
-          result2 = solution1([1, 2, 3, 4, 5]);
-    expect(result1).to.equal(null);
-    expect(result2).to.equal(null);
-  });
+it.only('should return null if there is no missing number', () => {
+const result1 = solution1([1,2,3,4,5]),
+      result2 = solution2([1,2,3,4,5]);
+      result3 = solution3([1,2,3,4,5]);
+expect(result1).to.equal(null);
+expect(result2).to.equal(null);
+expect(result3).to.equal(null);
+});
 
-  it('should return -8 for [-10,-9,-7,-6,-5]', () => {
-    const result1 = solution1([-10, -9, -7, -6, -5]);
-          result2 = solution1([-10, -9, -7, -6, -5]);
-    expect(result1).to.equal(-8);
-    expect(result2).to.equal(-8);
-  });
+it.only('should return -8 for [-10,-9,-7,-6,-5]', () => {
+const result1 = solution1([-10,-9,-7,-6,-5]),
+      result2 = solution2([-10,-9,-7,-6,-5]);
+      result3 = solution3([-10,-9,-7,-6,-5]);
+expect(result1).to.equal(-8);
+expect(result2).to.equal(-8);
+expect(result3).to.equal(-8);
+});
 });
 
